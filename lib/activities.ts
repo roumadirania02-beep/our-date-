7196acba4d07b4c4e08d8f638703ada2479d4bdf
roumadirania02-beep <roumadirania02@gmail.com>
2026-09-13 17:27:22 +0100

    Update activities.ts

--- lib/activities.ts
+++ lib/activities.ts
@@ -55,7 +55,7 @@ export const activities: Activity[] = [
     title: "Bowling",
     line: "Winner chooses the next thing 😌",
     image:
-      "https://plus.unsplash.com/premium_photo-1679321795639-c9b0c9f70abb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJvd2xpbmd8ZW58MHx8MHx8fDA%3D",
+      "https://images.unsplash.com/photo-1518620025093-fbca064bdd9a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGJvd2xpbmd8ZW58MHx8MHx8fDA%3D",
   },
   {
     id: "escape",
